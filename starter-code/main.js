e213d875b61578afda2e9c872636590e59fd95eb
aroberts1985 <adad516@gmail.com>
2017-02-26 19:45:02 -0500

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne="queen";
-var cardTwo="queen";
-var cardThree="king";
-var cardFour="king";
+//console.log("JS file is connected to HTML! Woo!")
+//var cardOne="queen";
+//var cardTwo="queen";
+//var cardThree="king";
+//var cardFour="king";
 
 
-if (cardOne === cardTwo) {
+
+/*if (cardOne === cardTwo) {
 	alert('You found a match!');
 } else if (cardThree === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+}*/
+var board = document.getElementById('game-board');
+
+
+function createboard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
